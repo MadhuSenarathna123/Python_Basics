3f4902881d8c751785d9904b4626a6c79e25ee1a
dmadhu123 <dulikamadushani1234@gmail.com>
2021-07-03 22:05:13 +0530

    initial commit

---
+++ Shapes.py
@@ -0,0 +1,21 @@
+print(" * "*10)
+print(" * "*10)
+print(" * "*10)
+print(" * "*10)
+print(" * "*10)
+
+print("\n")#break
+
+print(" * " * 5)
+print(" * " * 5)
+print(" * " * 5)
+print(" * " * 5)
+print(" * " * 5)
+
+print("\n")
+
+print(" "*5 + " * " )
+print(" "*4 + " * " * 2 )
+print(" "*3 + " * " * 3 )
+print(" "*2 + " * " * 4 )
+print(" "*1 + " * " * 5 )
---
+++ UserInput.py
@@ -0,0 +1,9 @@
+#Get User Input
+a = int(input("1st Number: "))
+b = int(input("2nd Number: "))
+
+#Add (a) & (b)
+c = a+b
+
+#Print Total
+print(c)
